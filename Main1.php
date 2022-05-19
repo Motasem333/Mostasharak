db0c75595a187ec9395a73246466680d552d5af3
AhmedM16 <ahmedmarwan105@gmail.com>
2022-05-20 02:23:59 +0300

    Main Page 90%

--- Main1.php
+++ Main1.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 
-
+<!--
 <?php
+/*
 $servername = "localhost";
 $username = "root";
 $pass = "";
@@ -22,21 +23,11 @@ else {
 	$run_num = mysqli_num_rows($run_select);
 
 }
-
-
-
-
-
-
-
-
-
-
-
+*/
 ?>
+-->
 <html lang="en" dir="ltr">
 <head>
-    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
@@ -65,16 +56,18 @@ else {
       <div class="container">
 <div class="alert alert-success" role="alert" style="margin-top:10vh; margin-bottom:2vh ;direction:rtl">
   <h4 class="alert-heading" style="direction:rtl; float:start">مرحبا بك !</h4>
+  <!---
   <?php
+  /*
    $email = $_POST['Email'];
    $select_name ="select F_name from user where Email = '$email'";
    $run_select1 = mysqli_query($conn,$select_name);
    while($result = mysqli_fetch_assoc($run_select1)){
    echo "<p>   <b>".$result['F_name']."</b>  تبين من خلال بياناتك التي ادخلتها ان هذا هو انسب برنامج غذائي لمدة 30 يوم القادمة </p>";}
-  ?>
+ */ ?>-->
   <hr>
 </div>
-            <div class="Sec1" style="margin-top:2vh ">
+            <div class="Sec1" style="margin-top:2vh ;  height: 195vh; ">
 			
                 <div class="head">
                     <h1 style="margin-bottom:5vh">الجدول الغذائي المقترح</h1>
@@ -91,7 +84,9 @@ else {
                             </tr>
                         </thead>
                         <tbody>
+                            <!---
 						<?php
+                        /*
 						if ( $run_num > 0){
 						while($row = mysqli_fetch_assoc($run_select)){
 						  echo "<tr>";									
@@ -109,7 +104,8 @@ else {
 							 echo "<td> "."<b> فارغ </b>"."</td>";
 							 echo "<td> "."<b> فارغ </b>"."</td>";	
 						}
-						?>
+                        */
+						?> -->
                         </tbody>
                     </table>
 					
@@ -123,7 +119,7 @@ else {
                         <h3>ألانجاز العام</h3>
                         <div class="progress">
                             <div class="progress-bar" id="progress-bar" role="progressbar" aria-valuenow="0"
-                            aria-valuemin="0" aria-valuemax="1500" style="width:499px">
+                            aria-valuemin="0" aria-valuemax="1500" style="width:999px">
                             </div>
                           </div>
                           <div class="Prpoint">
@@ -134,20 +130,69 @@ else {
                           <br><br>
                           <h5>-يتم الحصول على كود خصم بمقدار 25% عند انجاز 500 نقطة فأكثر</h5>
                           <h5>-يتم الحصول على هدية من اختيارك في حال الوصول الى 1000 نقطة في اقل من 50 يوم</h5>
-                          <button class="btn btn-outline-success btn-lg bg-white" id="Get1">طلب الحصول على كود</button>
+                          <h5>-استخدم كود الهدية او الخصم في المتجر لتفعيلهما </h5>
+                          <button class="btn btn-outline-success btn-lg bg-white" id="Get1">طلب الحصول على خصم</button>
                           <button class="btn btn-outline-success btn-lg bg-white" id="Get0" style="pointer-events:none">XXXXXX </button>
                           <img src="img/cancel.png"  id="cancel"alt="">
                           <br><br>
                           <button class="btn btn-outline-success btn-lg bg-white" id="Get2">طلب الحصول على هدية</button>
-
+                          <button class="btn btn-outline-success btn-lg bg-white" id="Get02" style="pointer-events:none ;z">XXXXXX </button>
+                          <img src="img/cancel.png"  id="cancel2" alt="">
 
                     </div>
                 </div>
             </div>
+        <div class="Sec2">
+            <div class="row"  style=" width:100%;justify-content: space-around;">
+                <!-- counter -->
+
+                <div id="Icon_hover" class="col-md-3 col-sm-6 bottom-margin text-center counter-section wow fadeInUp sm-margin-bottom-ten animated" data-wow-duration="300ms" style="visibility: visible; animation-duration: 300ms; animation-name: fadeInUp;">
+                  <img src="img\scale.png"  style="width:20vh"  alt="">
+                    <span id="anim-number-pizza" class="counter-number"></span>
+                    <span class="timer counter alt-font appear" data-to="980" data-speed="7000">24</span>
+                    <p class="counter-title" style="font-size:2rem">الكتلة الخاصة بجسمك</p>
+                </div>
+                <!-- end counter -->
+                <!-- counter -->
+                <div id="Icon_hover" class="col-md-3 col-sm-6 bottom-margin-small text-center counter-section wow fadeInUp xs-margin-bottom-ten animated" data-wow-duration="900ms" style="visibility: visible; animation-duration: 900ms; animation-name: fadeInUp;">
+                  <img src="img\apple.png"   style="width:20vh;"  alt="">
+                     <span class="timer counter alt-font appear" data-to="810" data-speed="7000">810</span>
+                    <span class="counter-title" style="font-size:2rem">السعرات الحرارية اليومية</span>
+                </div>
+                <!-- end counter -->
+                <!-- counter -->
+                <div id="Icon_hover" class="col-md-3 col-sm-6 text-center counter-section wow fadeInUp animated" data-wow-duration="1200ms" style="visibility: visible; animation-duration: 1200ms; animation-name: fadeInUp;">
+                  <img src="img\day.svg"  style="width:20vh"  alt="">
+                     <span class="timer counter alt-font appear" data-to="100" data-speed="7000">100</span>
+                    <span class="counter-title" style="font-size:2rem">   عدد الايام التي تم انجازها</span>
+                </div>
+                <!-- end counter -->
+              </div>
+              </div>
+
+
+
+              <div class="Sec3">
+                  <div class="Dailytopic1" >
+                      <h1>معلومة غذائية </h1>
+                      <p></p>
+                  </div>
+                  <div class="Dailytopic2">
+                    <h1> معلومة رياضية</h1>
+                    <p></p>
+                </div>
+              </div>
+
+
+
+
         </div>
+        </div>
+        
     
 </body>
 </html>
 
+<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"> </script>
 
-<script src="Main.js"></script>
+<script src="js/Main.js"></script>
